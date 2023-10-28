880af28f102a2f72f746e0e71ce69823a62f7ba8
dongyawu <31070209+dongyawu@users.noreply.github.com>
2023-10-28 23:33:43 +0800

    Update synpan_build.pl

--- synpan_build.pl
+++ synpan_build.pl
@@ -2,6 +2,8 @@
 use warnings;
 use stricts;
 
+####还没集成完
+
 ###note
 ###prot files should be named as SAMPLE.prot
 ###gff files should be named as SAMPLE.gff3
@@ -16,16 +18,50 @@ while (<IN>){
   chomp;
   $id=$_;
   if (-e $prot_dir."/".$_.prot"){
-    print $id.".prot is found";
+    print $id.".prot is found\n";
     system(qq(ln -s $prot_dir."/".$id.".prot" ./); 
   }
   else {
-    print $id."prot NO found!"
+    print $id.".prot NO found!\n";
     $countProtNF+=1;
   }
    if (-e $gff_dir."/".$_.gff3"){
-    print $id.".gff3 is found";
-    
+    print $id.".gff3 is found\n";
+    $gff=$id.".gff3";
+    open IN0,"$gff";
+    open OUT0,">$gff\_sim";
+    while (<IN0>){
+      chomp;
+      ($chr, $tools, $type, $start, $end, $dian, $strand, $other, $info)=split(/\t/,$_);
+      if ($type eq "mRNA"){
+      $info=~/ID=(.+);/;
+      $geneid=$1;
+      ### DAGchainer required format
+      print OUT0 $geneid."\t".$geneid."\t".$geneid."\t".$chr."\t".$start."\t".$end."\n";
+    }
+    close IN0;
+    close OUT0;
    }
-
+   else{
+     print $id.".gff3 NO found!\n";
+     $countGFFNF+=1;
+  }
 }
+
+if($countProtNF>0){
+  print "PROT FILES ARE INCOMPLETE!\n";
+}
+else {
+  print "PROT FILES ARE PREPARED~\n";
+}
+
+if($countGFFNF>0){
+  print "GFF (Coordinate) FILES ARE INCOMPLETE!\n";
+}
+else {
+  print "GFF (Coordinate) FILES ARE PREPARED~\n";
+}
+
+if 
+
+
